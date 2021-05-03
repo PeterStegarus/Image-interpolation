6b65a558386db6856dad274adf1f7dd966118c4e
Petre Stegarus <stegarusp@gmail.com>
2021-05-03 19:52:04 +0300

    a

--- bilinear/bilinear_coef.m
+++ bilinear/bilinear_coef.m
@@ -1,13 +1,17 @@
 function a = bilinear_coef(f, x1, y1, x2, y2)
-    % =========================================================================
-    % Calculeaza coeficientii a pentru interpolarea biliniara intre punctele
-    % (x1, y1), (x1, y2), (x2, y1) si (x2, y2)
-    % =========================================================================
-    
-    % TODO: calculeaza matricea A
-
-    % TODO: calculeaza vectorul b    
-
-    % TODO: calculeaza coeficientii
-
+  % =========================================================================
+  % Calculeaza coeficientii a pentru interpolarea biliniara intre punctele
+  % (x1, y1), (x1, y2), (x2, y1) si (x2, y2)
+  % =========================================================================
+  
+  % TODO: calculeaza matricea A
+  A = zeros(4);
+  A(:,1) = 1;
+  A(:,2) = [x1 x1 x2 x2]';
+  A(:,3) = [y1 y2 y1 y2]';
+  A(:,4) = A(:,2).*A(:,3);
+  % TODO: calculeaza vectorul b
+  b = [f(y1, x1); f(y2, x1); f(y1, x2); f(y2, x2)];
+  % TODO: calculeaza coeficientii
+  a = A\b;
 endfunction
--- bilinear/checker_props/checker-bilinear_4point.m
+++ bilinear/checker_props/checker-bilinear_4point.m
@@ -1,8 +1,8 @@
 %!test
 %! f = imread("../tests/in/points1.png");
 %! step = dlmread("../tests/in/points1_step.txt");
-%! img = bilinear_2x2(f, step);
-%! ref = dlmread("../tests/ref/bilinear/2x2_1.txt");
+%! img = bilinear_2x2(f, step)
+%! ref = dlmread("../tests/ref/bilinear/2x2_1.txt")
 %! assert(max(max(abs(ref - img))) <= 5);
 
 %!test
